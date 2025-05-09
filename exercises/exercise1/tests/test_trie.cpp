84bafb0486c2b80b929827831bc1382898656003
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-05-09 23:42:03 +0200

    update CMake to fetch content

--- exercises/exercise1/tests/test_trie.cpp
+++ exercises/exercise1/tests/test_trie.cpp
@@ -1,8 +1,9 @@
 #define CATCH_CONFIG_IMPLEMENTATION
-#include "catch_amalgamated.hpp"
+#include <catch2/catch_all.hpp>
 
 #include "Trie.h"
 
-TEST_CASE("hello() returns correct message", "[trie]") {
+TEST_CASE("hello() returns correct message", "[trie]") 
+{
     REQUIRE(hello() == "Hello from Trie");
 }
