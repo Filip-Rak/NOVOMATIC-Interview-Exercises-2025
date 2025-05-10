30e27a48da231b726f21934e9208e2fd93ce1923
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-05-10 16:01:01 +0200

    add test for is_colliding

--- exercises/exercise2/test_collision.cpp
+++ exercises/exercise2/test_collision.cpp
@@ -3,7 +3,56 @@
 
 #include "triangle.h"
 
-TEST_CASE("Dummy test case", "[collision]")
+TEST_CASE("Test 1: Identical triangles", "[is_colliding]")
 {
-	SUCCEED("Dummy test passed");
+	triangle t1 = { {{{0, 0}, {1, 0}, 0, 1}} };
+	triangle t2 = { {{{0, 0}, {1, 0}, 0, 1}} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE(colides);
+}
+
+TEST_CASE("Test 2: Triangles far apart", "[is_colliding]")
+{
+	triangle t1 = { {{{0, 0}, {1, 0}, 0, 1}} };
+	triangle t2 = { {{ {10, 10}, {11, 10}, {10, 11} }} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE_FALSE(colides);
+}
+
+TEST_CASE("Test 3: Partial overlap", "[is_colliding]")
+{
+	triangle t1 = { {{{0, 0}, {1, 0}, 0, 1}} };
+	triangle t2 = { {{ {0.5, 0.5}, {1.5, 0.5}, {0.5, 1.5} }} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE(colides);
+}
+
+TEST_CASE("Test 4: Touching on edge", "[is_colliding]")
+{
+	triangle t1 = { {{{0, 0}, {1, 0}, 0, 1}} };
+	triangle t2 = { {{ {0.5, 0.5}, {1.5, 0.5}, {0.5, 1.5} }} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE(colides);
+}
+
+TEST_CASE("Test 5: Single vertex overlap", "[is_colliding]")
+{
+	triangle t1 = { {{ {1, 1}, {2, 1}, {1, 2} }} };
+	triangle t2 = { {{ {1, 1}, {0, 2}, {2, 2} }} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE(colides);
+}
+
+TEST_CASE("Test 6: Close not overlapping", "[is_colliding]")
+{
+	triangle t1 = { {{ {4, 1}, {3, 1}, {2, 2} }} };
+	triangle t2 = { {{ {2, 1}, {1, 2}, {1, 3} }} };
+
+	bool colides = is_coliding(t1, t2);
+	REQUIRE_FALSE(colides);
 }
