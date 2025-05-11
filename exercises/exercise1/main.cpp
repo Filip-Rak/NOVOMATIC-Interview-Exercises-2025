c2afaebaa96112f1ba448ac9458c94da163280e2
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-05-11 16:55:04 +0200

    add header comment

--- exercises/exercise1/main.cpp
+++ exercises/exercise1/main.cpp
@@ -1,3 +1,24 @@
+/*	Exercise 1: szukajka.pl's search engine admin
+*	----------------------------------------------
+*	Autor: Filip Rak
+* 
+*	Description:
+*		Implements a simple search engine suggestion module using a Trie,
+*		allowing for prefix-based autocomplete.
+* 
+*	Input:
+*		- "add: [query]" -> Add the query to trie. 
+*		- "ask: [query]" -> Get all querries matching the prefix.
+*		- whitespaces between the end of the command and the start of the query will be ignored.
+* 
+*	Output: 
+*		- each [ask query] will be followed by every output mathcing the prefix
+*		  in format: result: [matching query]
+* 
+*	Notes:
+*     - Implementation is split into trie.h and trie.cpp in this directory.
+*/
+
 #include <iostream>
 #include <string>
 
@@ -6,22 +27,22 @@
 void handle_input(const std::string& line, Trie& trie)
 {
 	// Define commands.
-	const std::string ADD_COMMAND = "add:";
-	const std::string QUERY_COMMAND = "ask:";
+	const std::string COMMAND_ADD = "add:";
+	const std::string COMMAND_ASK = "ask:";
 
-	if (line.starts_with(ADD_COMMAND))
+	if (line.starts_with(COMMAND_ADD))
 	{
 		// Extract the query & eliminate whitespaces.
-		std::string query = line.substr(ADD_COMMAND.size());
+		std::string query = line.substr(COMMAND_ADD.size());
 		query.erase(0, query.find_first_not_of(' '));
 
 		// Execute the command.
 		trie.add_query(query);
 	}
-	else if (line.starts_with(QUERY_COMMAND))
+	else if (line.starts_with(COMMAND_ASK))
 	{
 		// Extract the query & eliminate whitespaces.
-		std::string query = line.substr(QUERY_COMMAND.size());
+		std::string query = line.substr(COMMAND_ASK.size());
 		query.erase(0, query.find_first_not_of(' '));
 
 		// Execute the command & print the result.
