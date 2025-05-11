c455350899a7c15a37248632b19fc38d042a94d2
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-05-11 12:03:03 +0200

    add solution to exercise 3

--- exercises/exercise3/main.cpp
+++ exercises/exercise3/main.cpp
@@ -2,7 +2,25 @@
 
 #include "calculate.h"
 
+struct Vec2 
+{
+	float x, y;
+	static Vec2 identity() { return { 0.f, 0.f }; }
+};
+
+auto add_vec2 = [](const Vec2& a, const Vec2& b) {
+	return Vec2{a.x + b.x, a.y + b.y}; };
+
+void run_example()
+{
+	Vec2 pos = {1.f, 2.f};
+	pos = calculate(3, pos, add_vec2);
+
+	std::cout << pos.x << " " << pos.y << "\n";
+}
+
+// Main function left minimal per spec. See tests for further usage examples.
 int main()
 {
-	std::cout << "Hello World!\n";
+	// run_example();
 }
