e9b8a5ed0d49b7bf9de28545bbcdd7e6fc384a50
Filip-Rak <73279456+Filip-Rak@users.noreply.github.com>
2025-05-11 18:41:00 +0200

    unify comment style

--- exercises/exercise1/trie.cpp
+++ exercises/exercise1/trie.cpp
@@ -8,23 +8,23 @@ Trie::Trie()
 
 void Trie::add_query(const std::string& query)
 {
-	// Process each character while advancing through nodes.
+	// Process each character while advancing through nodes
 	Node* tgt = root.get();
 	for (char c : to_lower_case(query))
 	{
-		// Character is present -> advance. Character not present -> map it to a new node -> advance.
+		// Character is present -> advance. Character not present -> map it to a new node -> advance
 		auto [it, _] = tgt->children.try_emplace(c, std::make_unique<Node>());
 		tgt = it->second.get();
 	}
 
-	// Mark the last inserted node as terminal.
+	// Mark the last inserted node as terminal
 	tgt->terminal = true;
 	tgt->full_query = query;
 }
 
 std::vector<std::string> Trie::get_prefixes(const std::string& prefix) const
 {
-	// Get to the end of the prefix.
+	// Get to the end of the prefix
 	Node* tgt = root.get();
 	for (char c : to_lower_case(prefix))
 	{
@@ -35,7 +35,7 @@ std::vector<std::string> Trie::get_prefixes(const std::string& prefix) const
 			return {};
 	}
 
-	// Using DFS gather all terminal nodes starting from the prefix's end.
+	// Using DFS gather all terminal nodes starting from the prefix's end
 	std::vector<std::string> results;
 	std::stack<const Node*> stack;
 	stack.push(tgt);
@@ -45,11 +45,11 @@ std::vector<std::string> Trie::get_prefixes(const std::string& prefix) const
 		const Node* current = stack.top();
 		stack.pop();
 
-		// Add terminal's node querry.
+		// Add terminal's node querry
 		if (current->terminal)
 			results.push_back(current->full_query);
 
-		// Add all of these nodes's children to stack.
+		// Add all of these nodes's children to stack
 		for (auto& [_, next_node] : current->children)
 			stack.push(next_node.get());
 	}
